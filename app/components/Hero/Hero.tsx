3f799eb0f3753bc1b15734fe4cf0f59cd1544492
mj-codices <mjwhite.dev@gmail.com>
2026-07-13 18:03:37 -0700

    Lowered brightness of hero video

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -4,14 +4,14 @@ export default function Hero() {
   return (
     <section className="relative h-svh w-full flex flex-col justify-center p-6 sm:p-12 md:p-16 xl:p-30 bg-stone-900 overflow-hidden">
       {/* Background Video with a dark overlay to make text pop */}
-      <div className="absolute inset-0 z-0">
+      <div className="absolute inset-0 z-0 hero-video-container">
         <video 
           src="/tire_01.mp4" /* Replace with your actual video path in the public folder */
           autoPlay 
           loop 
           muted 
           playsInline 
-          className="h-full w-full object-cover opacity-60"
+          className="h-full w-full object-cover opacity-60 hero-video-container"
         />
         <div className="absolute inset-0 bg-gradient-to-t from-stone-950 via-stone-900/40 to-transparent" />
       </div>
